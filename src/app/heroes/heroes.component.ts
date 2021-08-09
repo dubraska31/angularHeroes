4f9b722ebdb9f26801f3545daebffc859ebb85b5
dubraska31 <dubraskaprimi31@gmail.com>
2021-08-09 02:12:02 -0300

    version casi lista

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
